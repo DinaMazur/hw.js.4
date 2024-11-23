cc545bcda329a55fb16ee1033b23721de3802d6e
DinaMazur <146865201+DinaMazur@users.noreply.github.com>
2024-11-23 12:36:08 +0200

    Add files via upload

---
+++ hw4.js
@@ -0,0 +1,36 @@
+// const meaning = prompt(заповніть перше поле);
+// const meaningtwo = prompt(заповніть друге поле);
+// meaning !== "" && meaningtwo !== ""
+//     ? alert("Всі поля заповнені")
+//     : alert("Не всі поля заповнені");
+
+
+
+// const first = Number(prompt("введіть перше значення"));
+// const second = Number(prompt("введіть друге значення"));
+// let sum = "";
+// if (sum > 10) {
+//     alert('Сума більша за 10');
+// } else {
+//     alert("Сума менше або дорівнює 10");
+// }
+
+
+
+// const text = prompt("введіть текст");
+// if (text.includes("JavaScript")) {
+//     alert("В тексті є слово JavaScript")
+// } else {
+//     alert(("В тексті немає слова JavaScript"));
+// }
+
+
+
+// const num = Number(prompt("Введіть число"));
+// if (num > 10 && num < 20 ){
+//     alert("Число входить в діапазон від 10 до 20");
+
+// } else{ 
+//     alert( "Число не входить в діапазон від 10 до 20")
+// }
+
